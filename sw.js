1bc9cebebfbb886ed5b93f940f371b1fdd7bd11d
Ali Arbab <ali@aliarbab2009.com>
2026-04-26 22:44:23 +0530

    SW v206 close Hotfix50a (MF chart try-catch fallbacks)

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
 // Bump this on every deploy so old cached JS/HTML isn't served forever. The
 // activate step below deletes any cache whose name doesn't match. Include a
 // date so it is obvious in DevTools which build is live.
-const CACHE_NAME = "stocksaathi-v205-20260426ap";
+const CACHE_NAME = "stocksaathi-v206-20260426aq";
 const STATIC = [
   "./",
   "./index.html",
