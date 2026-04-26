d32c8391801817d89fc4fac47268df831d580bf6
Ali Arbab <ali@aliarbab2009.com>
2026-04-26 23:42:41 +0530

    SW v207 close Hotfix50b+c (error surface + MF zoom)

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
 // Bump this on every deploy so old cached JS/HTML isn't served forever. The
 // activate step below deletes any cache whose name doesn't match. Include a
 // date so it is obvious in DevTools which build is live.
-const CACHE_NAME = "stocksaathi-v206-20260426aq";
+const CACHE_NAME = "stocksaathi-v207-20260426ar";
 const STATIC = [
   "./",
   "./index.html",
