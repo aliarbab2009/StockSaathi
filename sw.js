327f4646998846ead895548eac7df6c752ce4a28
Ali Arbab <ali@aliarbab2009.com>
2026-04-27 01:38:42 +0530

    SW v213 close Hotfix55a (MF pill surgical update)

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
 // Bump this on every deploy so old cached JS/HTML isn't served forever. The
 // activate step below deletes any cache whose name doesn't match. Include a
 // date so it is obvious in DevTools which build is live.
-const CACHE_NAME = "stocksaathi-v212-20260427c";
+const CACHE_NAME = "stocksaathi-v213-20260427d";
 const STATIC = [
   "./",
   "./index.html",
