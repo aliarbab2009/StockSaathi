684311ec75cd0d6c9707d2da70942a7ccc0e2cb8
Ali Arbab <ali@aliarbab2009.com>
2026-05-03 11:00:27 +0530

    SW v229 to ship Hotfix64a-e (categories A-Z, Oil&Gas 44, mobile chart, weekend 1D)

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
 // Bump this on every deploy so old cached JS/HTML isn't served forever. The
 // activate step below deletes any cache whose name doesn't match. Include a
 // date so it is obvious in DevTools which build is live.
-const CACHE_NAME = "stocksaathi-v228-20260503a";
+const CACHE_NAME = "stocksaathi-v229-20260503b";
 const STATIC = [
   "./",
   "./index.html",
